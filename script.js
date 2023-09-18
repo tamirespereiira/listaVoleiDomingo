aa097d1dc44defdd8d6956625cc68b68ecb7d538
tamirespereiira <117820175+tamirespereiira@users.noreply.github.com>
2023-09-18 12:35:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,56 @@
-let contador = 1; // Inicializa o contador
+let listaPrincipal = [];
+let listaEspera = [];
 
-// Função para adicionar um número à lista
-function adicionarNumero() {
-  const lista = document.getElementById('numeroLista');
-
-  if (contador <= 28) {
-    const novoItem = document.createElement('li');
-    novoItem.textContent = contador;
-    lista.appendChild(novoItem);
-    contador++;
-  } else {
-    // Quando atingir 28, reinicie a contagem
-    const novoTitulo = document.createElement('h2');
-    novoTitulo.textContent = 'Lista de Espera';
-    lista.appendChild(novoTitulo);
-    contador = 1; // Reinicia o contador
-  }
+function adicionarNome() {
+    const nome = document.getElementById("novo-nome").value;
+    if (nome && listaPrincipal.length < 28) {
+        listaPrincipal.push(nome);
+        atualizarLista();
+        registrarAcao("Adicionado: " + nome);
+    }
 }
 
-// Chama a função ao carregar a página
-window.onload = adicionarNumero;
+function excluirNome(index) {
+    listaPrincipal.splice(index, 1);
+    atualizarLista();
+    registrarAcao("Excluído: " + listaPrincipal[index]);
+}
+
+function moverParaListaEspera(index) {
+    const nome = listaPrincipal[index];
+    listaEspera.push(nome);
+    excluirNome(index);
+    registrarAcao("Movido para Lista de Espera: " + nome);
+}
+
+function atualizarLista() {
+    const listaNomes = document.getElementById("lista-nomes");
+    listaNomes.innerHTML = "";
+    listaPrincipal.forEach((nome, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${index + 1}. ${nome}`;
+        listItem.appendChild(criarBotaoExcluir(index));
+        listItem.appendChild(criarBotaoMover(index));
+        listaNomes.appendChild(listItem);
+    });
+}
+
+function criarBotaoExcluir(index) {
+    const button = document.createElement("button");
+    button.textContent = "Excluir";
+    button.addEventListener("click", () => excluirNome(index));
+    return button;
+}
+
+function criarBotaoMover(index) {
+    const button = document.createElement("button");
+    button.textContent = "Mover para Lista de Espera";
+    button.addEventListener("click", () => moverParaListaEspera(index));
+    return button;
+}
+
+function registrarAcao(acao) {
+    const dataHora = new Date().toLocaleString();
+    console.log(dataHora + " - " + acao);
+    // Aqui você pode implementar a lógica para registrar no banco de dados.
+}
